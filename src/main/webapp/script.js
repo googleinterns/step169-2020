a7e22483f2adb0e0deb959ce66f47382b08d31f2
Maximilian Alfano-Smith <maxalfanosmith@gmail.com>
2020-07-27 23:12:12 +0000

    Fleshed out search suggestion functionality and merged some of Andrew's UI changes.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-<<<<<<< HEAD
 
 /**
     Runs when the page is first loaded. Does the following:
@@ -21,10 +20,10 @@
  */
 
 var sharedMap = null;
-
-    var map, places;
-    var autocomplete;
-    var countryRestrict = {'country': 'us'};
+var places;
+var autoComplete;
+var autoCompleteService;
+var countryRestrict = {'country': 'us'};
     
     
 function onPageLoad() {
@@ -56,18 +55,74 @@ function clearArticleList() {
     }
 }
 
+
 /**
-    Starting point of a region search. Submits query to the servlet and then passes its responses to the getRegionArticles function.
+    Begin regional news pipeline functions.
+    Two possible paths.
+    - Searches invoked by the search button in the form.
+    - Searched invoked by clicking on a suggested location.
+ */
+
+/**
+    Starting point of a region search that is invoked from the search button. Submits query to the servlet and then passes its responses to the getRegionArticles function.
  */
 function doSearch(form) {
     let region = form.elements["region-search-field"].value;
     if (region != "") {
         let topic = form.elements["topic-search-field"].value;
+        request = {'input' : region};
+        autoCompleteService.getPlacePredictions(request, ((predictions) => finishSearch(predictions, topic)));
+    } else {
+        /**
+            If we add a warning message later for invalid locations it could go here.
+         */
+    }
+}
+
+/**
+    Starting point of a region search that is not invoked by the search button. Submits query to the servlet and then passes its responses to the getRegionArticles function.
+ */
+function doSearchNotFromForm() {
+    let region = document.getElementById("region-search-field").value;
+    if (region != "") {
+        let topic = document.getElementById("topic-search-field").value;
         console.log("region: " + region + " topic: " + topic);
         let fetchParameter = "/region-news?region=" + region + "&topic=" + topic;
         const response = fetch(fetchParameter);
         response.then(getRegionArticles);
     } 
+}
+
+/**
+    Finishes the search using autocomplete suggested results.
+ */
+function finishSearch(suggestions, topic) {
+    let region = suggestions[0].description;
+    document.getElementById("region-search-field").value = region;
+    console.log("region: " + region + " topic: " + topic);
+    let fetchParameter = "/region-news?region=" + region + "&topic=" + topic;
+    const response = fetch(fetchParameter);
+    response.then(getRegionArticles);
+    const response2 = fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + region +"&key=AIzaSyDTrfkvl_JKE7dPcK3BBHlO4xF7JKFK4bY");
+    response2.then(getJSONOfGeoCoding);
+}
+
+/**
+    Get the region response geo coding.
+ */
+function getJSONOfGeoCoding(response) {
+    const json = response.json();
+    return json.then(moveToLocation);
+}
+
+/**
+    Moves the screen to the provided locaiton.
+ */
+function moveToLocation(json) {
+    if (json.results[0].geometry) {
+        sharedMap.panTo(json.results[0].geometry.location);
+        sharedMap.setZoom(10);
+    }
 }
 
 /**
@@ -77,6 +132,27 @@ function getRegionArticles(response) {
     const json = response.json();
     json.then(displayArticlesFromJSON);
 }
+
+
+/**
+    Displays the articles contained in the json to the webpage.
+ */
+function displayArticlesFromJSON(json) {
+    clearArticleList();
+    for (index in json) {
+        let articleObj = json[index];
+        addArticle(articleObj.title, articleObj.publisher, articleObj.description, articleObj.date, articleObj.url);
+    }
+    openNav();
+}
+
+/**
+    End regional news pipeline functions.
+ */
+
+/**
+    Begin world news pipeline functions.
+ */
 
 /**
     Retrieves the json from the world-news servlet response.
@@ -102,14 +178,14 @@ function configureWorldArticles(json) {
     for (key in articleMap) {
         console.log(key, articleMap[key].length);
         response = fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + key +"&key=AIzaSyDTrfkvl_JKE7dPcK3BBHlO4xF7JKFK4bY");
-        response.then(getJSONOfGeoCoding.bind(null, articleMap[key]));
+        response.then(getRegionJSONOfGeoCoding.bind(null, articleMap[key]));
     }
 }
 
 /**
-    Prints the response.
+    Get the region response geo coding.
  */
-function getJSONOfGeoCoding(articles, response) {
+function getRegionJSONOfGeoCoding(articles, response) {
     const json = response.json();
     return json.then(placeArticlesPinOnMap.bind(null, articles));
 }
@@ -126,15 +202,8 @@ function placeArticlesPinOnMap(articles, json) {
 }
 
 /**
-    Displays the articles contained in the json to the webpage.
+    End world news pipeline functions.
  */
-function displayArticlesFromJSON(json) {
-    clearArticleList();
-    for (index in json) {
-        let articleObj = json[index];
-        addArticle(articleObj.title, articleObj.publisher, articleObj.description, articleObj.date, articleObj.url);
-    }
-}
 
 /**
     Fetches the initial articles displayed on the page.
@@ -158,16 +227,6 @@ function getInitialContent() {
 }
 
 /**
-    Function for testing that adds articles with a custom title.
- */
-function testAddArticles(title) {
-    clearArticleList();
-    for (i = 1; i <= 10; i++) {
-        addArticle(title + " " + i, "Publisher " + i, "Content " + i, "07/23/2020", "https://www.google.com");
-    }
-}
-
-/**
     Displays the passed list of articles.
  */
 function displayArticles(articles) {
@@ -176,6 +235,7 @@ function displayArticles(articles) {
         articleObj = articles[i];
         addArticle(articleObj.title, articleObj.publisher, articleObj.description, articleObj.date, articleObj.url);
     }
+    openNav();
 }
 
 /**
@@ -482,32 +542,43 @@ function initMap() {
 
 function initAutoComplete() {
 
-    // Create the autocomplete object and associate it with the UI input control.
+    // Create the autoComplete object and associate it with the UI input control.
     // Restrict the search to the default country, and to place type "cities".
-    autocomplete = new google.maps.places.Autocomplete(
+    autoComplete = new google.maps.places.Autocomplete(
         /** @type {!HTMLInputElement} */ (
             document.getElementById('region-search-field')), {
             types: ['(cities)'],
             componentRestrictions: countryRestrict
         });
     places = new google.maps.places.PlacesService(map);
+    autoCompleteService = new google.maps.places.AutocompleteService();
+    autoComplete.addListener('place_changed', onPlaceChanged);
 
-    autocomplete.addListener('place_changed', onPlaceChanged);
 
     // Add a DOM event listener to react when the user selects a country.
     document.getElementById('country').addEventListener(
-        'change', setAutocompleteCountry);
+        'change', setAutoCompleteCountry);
+
     }
 
     // When the user selects a city, get the place details for the city and
     // zoom the map in on the city.
 function onPlaceChanged() {
-    var place = autocomplete.getPlace();
+    var place = autoComplete.getPlace();
+    console.log(place);
     if (place.geometry) {
-        map.panTo(place.geometry.location);
-        map.setZoom(10);
-        search();
+        sharedMap.panTo(place.geometry.location);
+        sharedMap.setZoom(10);
+        doSearchNotFromForm();
     } else {
         document.getElementById('region-search-field').placeholder = 'Enter a city';
     }
+}
+
+function openNav() {
+    document.getElementById("article-list-container").style.width = "30vw";
+}
+
+function closeNav() {
+    document.getElementById("article-list-container").style.width = "0";
 }
