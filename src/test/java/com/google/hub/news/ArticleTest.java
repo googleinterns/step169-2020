4181f5e2dcaae83fa589bd84fdd5c75ddca1ed92
Dylan Weber <dylanweber214@gmail.com>
2020-08-29 00:01:42 +0000

    Add inequality checks for Article and Location classes

--- src/test/java/com/google/hub/news/ArticleTest.java
+++ src/test/java/com/google/hub/news/ArticleTest.java
@@ -25,6 +25,20 @@ public final class ArticleTest {
   private static final Article ARTICLE_1 = new Article(TITLE_1, 
       PUBLISHER_1, DATE_1, DESCRIPTION_1, URL_1, THUMBNAIL_URL_1, LOCATION_1, THEME_1);
   
+  private static final String TITLE_2 = "The New Jeep Wagoneer Will Apparently Have A Map Of Detroit Imprinted In Its Panoramic Roof - Jalopnik";
+  private static final String PUBLISHER_2 = "Jalopnik";
+  private static final Instant DATE_2 = Instant.parse("2020-08-28T18:25:00Z");
+  private static final String DESCRIPTION_2 = "The long-overdue"
+    + "three-row Jeep, the Jeep Wagoneer, shows its face on" 
+    + " September 3. Until then, you can expect to continue"
+    + " seeing teasers from Jeep, with the latest two being the"
+    + " shot above showing the SUV’s panoramic glass roof and" 
+    + " another one showing a nice,…";
+  private static final String URL_2 = "https://jalopnik.com/the-new-jeep-wagoneer-will-apparently-have-a-map-of-det-1844882140";
+  private static final String THUMBNAIL_URL_2 = "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/gzvvneupzrg5wzkfu5kd.png";
+  private static final Location LOCATION_2 = new Location("Detroit", "Michigan", "USA");
+  private static final String THEME_2 = "business";
+
   @Test
   public void constructorSetsAppropriateFields() {
     Article article = new Article(TITLE_1, PUBLISHER_1, DATE_1, DESCRIPTION_1,
@@ -50,5 +64,61 @@ public final class ArticleTest {
     Article article2 = new Article(TITLE_1, 
       PUBLISHER_1, DATE_1, DESCRIPTION_1, URL_1, THUMBNAIL_URL_1, LOCATION_1, THEME_1); // Should be identical to ARTICLE_1
     Assert.assertEquals(ARTICLE_1, article2);
+  }
+
+  @Test
+  public void unequalTitle() {
+    Article article2 = new Article(TITLE_2, 
+      PUBLISHER_1, DATE_1, DESCRIPTION_1, URL_1, THUMBNAIL_URL_1, LOCATION_1, THEME_1); // Should be identical to ARTICLE_1
+    Assert.assertNotEquals(ARTICLE_1, article2);
+  }
+
+  @Test
+  public void unequalPublisher() {
+    Article article2 = new Article(TITLE_1, 
+      PUBLISHER_2, DATE_1, DESCRIPTION_1, URL_1, THUMBNAIL_URL_1, LOCATION_1, THEME_1); // Should be identical to ARTICLE_1
+    Assert.assertNotEquals(ARTICLE_1, article2);
+  }
+
+  @Test
+  public void unequalDate() {
+    Article article2 = new Article(TITLE_1, 
+      PUBLISHER_1, DATE_2, DESCRIPTION_1, URL_1, THUMBNAIL_URL_1, LOCATION_1, THEME_1); // Should be identical to ARTICLE_1
+    Assert.assertNotEquals(ARTICLE_1, article2);
+  }
+
+  @Test
+  public void unequalDescription() {
+    Article article2 = new Article(TITLE_1, 
+      PUBLISHER_1, DATE_1, DESCRIPTION_2, URL_1, THUMBNAIL_URL_1, LOCATION_1, THEME_1); // Should be identical to ARTICLE_1
+    Assert.assertNotEquals(ARTICLE_1, article2);
+  }
+
+  @Test
+  public void unequalUrl() {
+    Article article2 = new Article(TITLE_1, 
+      PUBLISHER_1, DATE_1, DESCRIPTION_1, URL_2, THUMBNAIL_URL_1, LOCATION_1, THEME_1); // Should be identical to ARTICLE_1
+    Assert.assertNotEquals(ARTICLE_1, article2);
+  }
+
+  @Test
+  public void unequalThumbnailUrl() {
+    Article article2 = new Article(TITLE_1, 
+      PUBLISHER_1, DATE_1, DESCRIPTION_1, URL_1, THUMBNAIL_URL_2, LOCATION_1, THEME_1); // Should be identical to ARTICLE_1
+    Assert.assertNotEquals(ARTICLE_1, article2);
+  }
+
+  @Test
+  public void unequalLocation() {
+    Article article2 = new Article(TITLE_1, 
+      PUBLISHER_1, DATE_1, DESCRIPTION_1, URL_1, THUMBNAIL_URL_1, LOCATION_2, THEME_1); // Should be identical to ARTICLE_1
+    Assert.assertNotEquals(ARTICLE_1, article2);
+  }
+
+  @Test
+  public void unequalTheme() {
+    Article article2 = new Article(TITLE_1, 
+      PUBLISHER_1, DATE_1, DESCRIPTION_1, URL_1, THUMBNAIL_URL_1, LOCATION_2, THEME_2); // Should be identical to ARTICLE_1
+    Assert.assertNotEquals(ARTICLE_1, article2);
   }
 }
--- src/test/java/com/google/hub/news/LocationTest.java
+++ src/test/java/com/google/hub/news/LocationTest.java
@@ -15,6 +15,9 @@ public final class LocationTest {
   private static final Location LOCATION_1 = new Location(CITY_1, 
       SUBCOUNTRY_1, 
       COUNTRY_1);
+  private static final String CITY_2 = "Toronto";
+  private static final String SUBCOUNTRY_2 = "Ontario";
+  private static final String COUNTRY_2 = "Canada";
 
   @Test
   public void constructorSetsAppropriateFields() {
@@ -36,5 +39,29 @@ public final class LocationTest {
       SUBCOUNTRY_1, 
       COUNTRY_1); // Should be identical to LOCATION_1
     Assert.assertEquals(LOCATION_1, location2);
+  }
+
+  @Test
+  public void unequalCity() {
+    Location location2 = new Location(CITY_2, 
+      SUBCOUNTRY_1, 
+      COUNTRY_1); // Should be identical to LOCATION_1 besides city
+    Assert.assertNotEquals(LOCATION_1, location2);
+  }
+
+  @Test
+  public void unequalSubcountry() {
+    Location location2 = new Location(CITY_1, 
+      SUBCOUNTRY_2, 
+      COUNTRY_1); // Should be identical to LOCATION_1 besides subcountry
+    Assert.assertNotEquals(LOCATION_1, location2);
+  }
+
+  @Test
+  public void unequalCountry() {
+    Location location2 = new Location(CITY_1, 
+      SUBCOUNTRY_1, 
+      COUNTRY_2); // Should be identical to LOCATION_1 besides country
+    Assert.assertNotEquals(LOCATION_1, location2);
   }
 }
